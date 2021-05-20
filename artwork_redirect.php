163e807f9b4663b2496621c2608f29943c3e80de
Milestones of Early Cognitive Development <milestones-online@cbs.mpg.de>
2021-05-20 21:48:13 +0200

    artwork_redirect mobile

---
+++ artwork_redirect.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Artwork | Clara Schüler</title>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum- 
+scale=1.0; user-scalable=0">
+<link rel="icon" href="/media/favicon.ico" type="image/x-icon">
+</head>
+<frameset rows="100%" border="0" framespacing="0" frameborder="0">
+<frame name="main" src="http://clara.sprout.photo/" marginwidth="0" 
+marginheight="0" scrolling="auto" 
+noresize="noresize">
+</frameset>
+<noframes>
+</noframes>
+</html>
